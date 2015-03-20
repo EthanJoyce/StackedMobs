b3fa6d4b42d4288b0bace4acd4e3ec085b5e5c66
MrLolEthan <me@mrlolethan.com>
2015-03-20 10:45:19 -0230

    Initial commit.

---
+++ src/main/java/com/mrlolethan/stackedmobs/P.java
@@ -0,0 +1,19 @@
+package com.mrlolethan.stackedmobs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class P extends JavaPlugin {
+
+	@Override
+	public void onLoad() {
+	}
+
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+	}
+
+}
